f50bd965ce4dd4b3a7e60805c177b0be475853c3
ljiahao <jh_liu@zju.edu.cn>
2020-09-16 21:24:19 +0800

    2nd machine

--- handleDir.py
+++ handleDir.py
@@ -3,16 +3,16 @@ import os
 
 
 def main():
-	dir_set = [63, 46, 31, 10, 64, 74, 78, 32]
-	subprocess.call('mkdir res101', shell = True)
+	dir_set = [10]
+	#subprocess.call('mkdir res101', shell = True)
 	for d in dir_set:
 		path = './' + str(d)
 		file1 = path + "/qemu.initial.serial.log"
 		file2 = path + "/qeum.final.serial.log"
 		os.rename(file1, str(d) + '_initial.log')
-		subprocess.call('cp ' + str(d) + '_initial.log' + ' ./res101/', shell = True)
+		subprocess.call('cp ' + str(d) + '_initial.log' + ' ./firmadyne_log/2th/', shell = True)
 		os.rename(file2, str(d) + "_final.log")
-		subprocess.call('cp ' + str(d) + '_final.log' + ' ./res101/', shell = True)
+		subprocess.call('cp ' + str(d) + '_final.log' + ' ./firmadyne_log/2th/', shell = True)
 
 if __name__ == '__main__':
 	main()
