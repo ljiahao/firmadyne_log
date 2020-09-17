9e37abf450d6ff6602b0bf9d64ca848996d57a93
ljiahao <jh_liu@zju.edu.cn>
2020-09-17 10:31:39 +0800

    aa“

--- handleDir.py
+++ handleDir.py
@@ -3,16 +3,16 @@ import os
 
 
 def main():
-	dir_set = [368, 100, 97, 61, 221, 504, 405, 358, 501, 39, 299, 450, 565, 547, 91, 188, 484, 367, 4, 165, 254, 108, 302, 402, 60, 446]
+	dir_set = [173, 94, 164, 7, 3, 189, 21, 10, 77, 99, 167, 45, 59, 22, 182]
 	#subprocess.call('mkdir res101', shell = True
 	for d in dir_set:
 		path = './' + str(d)
 		file1 = path + "/qemu.initial.serial.log"
 		file2 = path + "/qemu.final.serial.log"
 		os.rename(file1, str(d) + '_initial.log')
-		subprocess.call('cp ' + str(d) + '_initial.log' + ' ./firmadyne_log/8th/', shell = True)
+		subprocess.call('cp ' + str(d) + '_initial.log' + ' ./firmadyne_log/9th/', shell = True)
 		os.rename(file2, str(d) + "_final.log")
-		subprocess.call('cp ' + str(d) + '_final.log' + ' ./firmadyne_log/8th/', shell = True)
+		subprocess.call('cp ' + str(d) + '_final.log' + ' ./firmadyne_log/9th/', shell = True)
 
 
 if __name__ == '__main__':
